9eefd9a00b5c103ce935e09199ec3591e2bbc41a
NRaeewal <39168910+NRaeewal@users.noreply.github.com>
2021-05-19 19:53:53 -0600

    oauth complete

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+
+var passport = require('passport');
+
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+
 
 //load envrronment variables
 require('dotenv').config();
@@ -15,6 +22,10 @@ var app = express();
 //mongodb connection
 require('./config/database');
 
+//passport connection
+require('./config/passport');
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -23,6 +34,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'SEIRocks!',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
