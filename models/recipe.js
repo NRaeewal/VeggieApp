047249eeb2320260c24cf1a08a3a6539dcbeaf63
NRaeewal <39168910+NRaeewal@users.noreply.github.com>
2021-05-21 16:34:44 -0600

    accepted changes

--- models/recipe.js
+++ models/recipe.js
@@ -2,15 +2,7 @@ const mongoose = require('mongoose');
 
 
 const Schema = mongoose.Schema;
-	
-<<<<<<< HEAD
-const movieSchema = new Schema({
-  title: String,
-  ingredients: [String],
-  procedure: String,
-    
-});
-=======
+
 const recipeSchema = new Schema({
   name: String,
   ingredients: [String],
@@ -18,4 +10,3 @@ const recipeSchema = new Schema({
 });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
->>>>>>> 7b38908e01cbe78493f1612fca96666c72dc1418
